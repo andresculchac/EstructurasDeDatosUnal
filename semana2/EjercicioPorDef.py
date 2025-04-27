cea071ec82ea594d922b9b4d72f409c99e1af2c8
Andres Yamith <mompyrri2001@gmail.com>
2025-04-27 13:32:20 -0500

    Add exercise1, and upload effiency code

--- semana2/EjercicioPorDef.py
+++ semana2/EjercicioPorDef.py
@@ -1,35 +1,23 @@
-def sumasTotales(list, intIntentos):
-    sumatorias = []
-    jugadores = []
-    so = 0
-    lar = 0
-    iS = 0
-    for i in range(intIntentos): 
-        contador = 0
-        for j in range(3):#Son 3 participantes
-            sumaMainTuple = list[j][i]
-            jugadores.append(isPar(sumaMainTuple)) # Agregar la suma de cada columna a la lista
-            contador += sumaMainTuple
-        sumatorias.append(isPar(contador)) # Agregar la suma de cada columna a la lista
-    for i in range(3): # Son 3 participantes
-        for s in sumatorias:
-            if i == 0:
-                if jugadores[0] and s:
-                    so += 1
-                elif jugadores[0] == False and s == False:
-                    so += 1
-            if i == 1:
-                if jugadores[1] and s:
-                    lar += 1
-                elif jugadores[1] == False and s == False:
-                    lar += 1
-            if i == 2:
-                if jugadores[2] and s:
-                    iS += 1
-                elif jugadores[2] == False and s == False:
-                    iS += 1
+#Quiero simplificar el programa, ademas de encontrar el error que me da en las sumas
+#Entiendo porque un codigo largo es dificil de leer, la importancia de hacer un codigo limpio y entendible
+#so lar is. Estoy creando 3 variables, pero puedo simplificarlo en una lista
 
-    return so, lar, iS
+def sumasTotales(list, intIntentos): 
+    ptsJugadores = [0,0,0]
+    
+    for i in range(intIntentos):
+        sumatorias = [] 
+        for j in range(3):
+            sumaMainTuple = list[j][i]
+            sumatorias.append(sumaMainTuple)
+        paridad = isPar(sum(sumatorias)) #necesito recorrer para los tres jugadores
+
+        for y in range(3):
+            intentoJugador = isPar(sumatorias[y])
+            if paridad == intentoJugador:
+                ptsJugadores[y] += 1
+    
+    return ptsJugadores[0], ptsJugadores[1], ptsJugadores[2] #SO, LAR, IS
 def isPar(num):
     if num % 2 == 0:
         return True
@@ -40,9 +28,12 @@ def isPar(num):
 rInput = int(input())
 mainTuple = []
 
-for i in range(3): # Son 3 participantes
-    c = list(map(int, input().split(",")))
+for i in range(3): #Recibir la entrada predeterminada (3 participantes)
+    c = list(map(int, input().split(", ")))
     mainTuple.append(c)
+
+
+
 
 so, lar, iS = sumasTotales(mainTuple,rInput)
 print(f"SO:{so}, LAR:{lar}, IS:{iS}")
