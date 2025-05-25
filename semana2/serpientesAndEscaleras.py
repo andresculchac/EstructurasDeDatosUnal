7a65e4c7a21f551c7d6f4f19bb47d8b1c4121a30
YamithAndres <mompyrri2001@gmail.com>
2025-05-24 20:10:24 -0500

    Entradas con chatGPT

--- semana2/serpientesAndEscaleras.py
+++ semana2/serpientesAndEscaleras.py
@@ -1,18 +1,25 @@
-turnos = 0
-index = set()   #para encontrar respuestas O(1)
-listTurn = [3, -1, 4, -2]
-initialTurn = 0
-while True:
-    algorithm =  initialTurn + listTurn[initialTurn]
-    #comencemos por los turnos
-    if algorithm < 0 or algorithm >= len(listTurn):
-        break
-    #agregar el if cuando se repite la casilla
-    if algorithm in index:
-        break
-    turnos += 1 #contabilizamos turnos
-    #rapidamente nos ponemos en la posicion de la primera lista, falta lo interactivo
-    index.add(algorithm)
-    initialTurn = algorithm
-print(turnos)    
-     
+def Solve():
+    CaseCount = int(input())
+    Answers = []
+    for _ in range(CaseCount):
+        TurnQty = int(input())
+        ListTurn = list(map(int, input().split()))
+        TurnCounter = 0
+        Visited = set()
+        Current = 0
+        while True:
+            Next = Current + ListTurn[Current]
+            if Next < 0 or Next >= len(ListTurn):
+                break
+            if Next in Visited:
+                break
+            TurnCounter += 1
+            Visited.add(Next)
+            Current = Next
+        Answers.append(TurnCounter)
+    for Value in Answers:
+        print(Value)
+
+
+if __name__ == "__main__":
+    Solve()
