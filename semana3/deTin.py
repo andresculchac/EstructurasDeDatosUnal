6335280291b82293bb81fdf9d336da0bf3df8f6b
YamithAndres <mompyrri2001@gmail.com>
2025-07-11 22:14:31 -0500

    Este es el punto en el que salen algunos puntos, pero no sale el último, por eso me parece importante guardar este commit

--- semana3/deTin.py
+++ semana3/deTin.py
@@ -1,6 +1,27 @@
 def tin(N,K):
     students = [i for i in range(1,N+1)] #[1,2,3,4,5]
-    while K != 0:
-        return students
+    inicio = 0
+    while N != 1:
+        u = (inicio + K) % N
+        inicio = u-1
+        cleanP = students.pop(inicio)   
+        N = len(students)
+        K = cleanP % N
+        if K == 0:
+            K = 1
+    return students[0]
 
-print(tin(5,3))
+
+nCases = int(input())
+
+for _ in range(nCases):
+    a = list(map(int, input().split()))
+    resultado = tin(a[0],a[1])
+    print(resultado)
+
+
+
+
+
+
+
