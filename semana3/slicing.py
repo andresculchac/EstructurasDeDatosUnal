2239ca34d4b590e8ff167cfa9cfb3354d82e6f6c
YamithAndres <mompyrri2001@gmail.com>
2025-07-14 16:20:31 -0500

    Add slicing.py with list slicing examples and indicateNumbers function

---
+++ semana3/slicing.py
@@ -0,0 +1,31 @@
+my_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+
+
+# # sequence[start:stop:step]
+# # Get elements from index 2 to 5 (exclusive)
+# print(my_list[2:5])  # Output: [2, 3, 4]
+
+# # Get elements from beginning to index 4
+# print(my_list[:5])   # Output: [0, 1, 2, 3, 4]
+
+# # Get elements from index 5 to end
+# print(my_list[5:])   # Output: [5, 6, 7, 8, 9]
+
+# # Get every other element
+# print(my_list[::2])  # Output: [0, 2, 4, 6, 8]
+
+longM = len(my_list) 
+
+
+
+def indicateNumbers (i,j):
+    if i>0 and j<longM+1:
+        mTrue = my_list[(i-1):(j)]
+        for i in mTrue:
+            print(i, end=" ")
+
+
+# for i in range(-4,0,1):
+#     print(i)
