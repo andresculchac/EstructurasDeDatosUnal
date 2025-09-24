bb35940f259f4853d9d7907ffce73008bb36a4de
YamithAndres <mompyrri2001@gmail.com>
2025-09-24 18:05:16 -0500

    Refactor findingMedian.py to improve median calculation logic and streamline output handling

--- findingMedian.py
+++ findingMedian.py
@@ -1,21 +1,27 @@
 multiple = int(input())
 counter = 0
 numbers = []
+container = []
 while True: 
-    counter += 1
     number = int(input())
     if number == 0:
         break
+    counter += 1
     numbers.append(number)
-    if counter % 5 == 0:
+    if counter % multiple == 0:
         numbers.sort()
         longNumbers = len(numbers)
-        if longNumbers % 2 != 0:        #find the medium index
+        if longNumbers % 2 != 0: #Odd        #find the medium index
             median = numbers[longNumbers//2]
-            print(median)
+            container.append(median)
         else:
-            median = numbers[int(longNumbers)/2] + numbers[(int(longNumbers)/2)+1]
-            print(median,'/2')
+            median = numbers[longNumbers//2 - 1 : longNumbers//2 + 1]
+            sumFind = sum(median)
+            if sumFind % 2 == 1:
+                medianFinding =str(sum(median))+"/2"
+                container.append(medianFinding)
+            else:
+                container.append(sumFind/2)
 
-
-
+for i in container:
+    print(str(i))
